08eb899c25a03901af2e56869d79743b7157d8a2
faandg <faandg@gmail.com>
2013-11-18 16:16:33 +0100

    Initial commit

---
+++ src/main/java/com/programmeren4/adventuregame/App.java
@@ -0,0 +1,13 @@
+package com.programmeren4.adventuregame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
